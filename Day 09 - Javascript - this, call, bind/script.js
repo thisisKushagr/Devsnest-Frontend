4ca3895e3ee4abdcd74bd261cb40f6dd90798393
Kushagra Goel <goel.k874@gmail.com>
2021-06-26 14:14:28 +0530

    day_9

--- Day 09 - Javascript - this, call, bind/script.js
+++ Day 09 - Javascript - this, call, bind/script.js
@@ -1,5 +1,33 @@
-var a = 10;
-function ab(){
-    console.log("Hello World!");
+console.log("script.js is ONLINE🟢");
+
+let obj1 = {
+    firstName : "Wingardium",
+    lastName : "Leviosa",
+    user : "Ron",
+    fullNameArrow1 : () => {  
+        console.log(this);
+        console.log(this.firstName + " " + this.lastName)
+    },
+    fullNameFunction1 : function() {  
+        console.log(this);
+        console.log(this.firstName + " " + this.lastName)
+    }, 
+    getThis: () => { console.log(this)}
+};
+
+let obj2 = {
+    firstName : "Avada",
+    lastName : "Kedavra",
+    generalUser : "Voldemort",
+    fullNameArrow2 : () => {
+        console.log(this.firstName + " " + this.lastName)
+    },
+    fullNameFunction2 : function() { 
+        console.log(this);
+        return (this.firstName + " " + this.lastName)
+    }, 
+};
+
+var bindedFunction = () => {
+    console.log(object2.fullNameFunction2());
 }
-console.log(window);
